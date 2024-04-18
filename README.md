0ab959759f746c8c78c02ae9d22c7a256073a563
iwatkot <iwatkot@gmail.com>
2024-04-18 14:26:55 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# script-example
+1. Edit `local.env` file.
+2. Add your code after API initialization in `src.main.py`.
+3. Launch the script.
---
+++ src/main.py
@@ -0,0 +1,21 @@
+import os
+
+import supervisely as sly
+from dotenv import load_dotenv
+from supervisely.app.widgets import Card
+
+# Ensure that supervisely.env contains SERVER_ADDRESS and API_TOKEN.
+load_dotenv(os.path.expanduser("~/supervisely.env"))
+# Ensure that local.env contains TEAM_ID and WORKSPACE_ID.
+load_dotenv("local.env")
+
+team_id = sly.io.env.team_id()
+workspace_id = sly.io.env.workspace_id()
+
+api: sly.Api = sly.Api.from_env()
+
+print(f"API instance created for team_id={team_id}, workspace_id={workspace_id}")
+
+card = Card("Hello, world!", "Add widgets here.")
+
+app = sly.Application(layout=card)
