c54f9f192b2cd7d640c3300350f6eb438a4adc8d
iwatkot <iwatkot@gmail.com>
2024-09-04 13:12:59 +0200

    WIP.

--- src/main.py
+++ src/main.py
@@ -7,13 +7,6 @@ from src.ui.settings import card
 app = sly.Application(layout=card)
 
 
-# region list of checks
-# 1. Check if annotaion on image does not contain any objects.
-# 2. Check if annotation on image does not contain all objects from the project meta.
-
-# endregion
-
-
 @app.event(sly.Event.JobEntity.StatusChanged)
 @sly.timeit
 def job_status_changed(api: sly.Api, event: sly.Event.JobEntity.StatusChanged):
